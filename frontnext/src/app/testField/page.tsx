8841017c72e7732cff51f9f9bedc47d8e0fe612d
김정수 <kjs20151240@gmail.com>
2024-07-26 11:06:02 +0900

    :bulb: 김정수 : code수정

--- frontnext/src/app/testField/page.tsx
+++ frontnext/src/app/testField/page.tsx
@@ -1,1 +1,12 @@
-console.log('testField');
+import Image from 'next/image';
+import '../globals.css';
+import Message from '../ui/testComponent/InputMessage';
+import { styles } from '../ui/testComponent/stylesContent';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Message message="Hellow World" isSuccess={true} />
+    </div>
+  );
+}
