6ede6de23ec907fcdda0a3030fc58eacf04421c2
김정수 <kjs20151240@gmail.com>
2024-07-26 12:33:08 +0900

    :recycle: 김정수 : 리터럴값 삭제

--- frontnext/src/app/ui/TestHeader.tsx
+++ frontnext/src/app/ui/TestHeader.tsx
@@ -2,25 +2,26 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import styles from '../headerStyles.module.css'; // CSS 모듈 파일을 import
+import styles from '../headerStyles.module.css'; // CSS 모듈을 import
+import { TESTROUTES, TABMENUTEXTS } from './testComponent/routerPath';
 
 const TestHeader: React.FC = () => {
   const router = useRouter();
 
-  const handleTabClick = (path: string) => {
-    router.push(path);
+  const handleTabClick = (routeKey: keyof typeof TESTROUTES) => {
+    router.push(TESTROUTES[routeKey]);
   };
 
   return (
     <header className={styles.header}>
-      <div className={styles.tab} onClick={() => handleTabClick('/testField')}>
-        조회
+      <div className={styles.tab} onClick={() => handleTabClick('TAB_ONE')}>
+        {TABMENUTEXTS.TABMENUONE}
       </div>
-      <div className={styles.tab} onClick={() => handleTabClick('/testCreate')}>
-        생성
+      <div className={styles.tab} onClick={() => handleTabClick('TAB_TWO')}>
+        {TABMENUTEXTS.TABMENUTWO}
       </div>
-      <div className={styles.tab} onClick={() => handleTabClick('/testSet')}>
-        설정
+      <div className={styles.tab} onClick={() => handleTabClick('TAB_THREE')}>
+        {TABMENUTEXTS.TABMENUTHREE}
       </div>
     </header>
   );
