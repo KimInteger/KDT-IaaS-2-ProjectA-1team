7c749ee0d21948301fa8a305501006c4338a34a5
김정수 <kjs20151240@gmail.com>
2024-07-26 10:30:09 +0900

    :bulb: 김정수 : login check modal

--- frontnext/src/app/ui/modalComponent/loginCheckFalseModal.tsx
+++ frontnext/src/app/ui/modalComponent/loginCheckFalseModal.tsx
@@ -3,15 +3,18 @@
 import React from 'react';
 
 interface ModalProps {
-  message: string;
+  alertmessage: string;
   onClose: () => void;
 }
 
-const LoginCheckFalseModal: React.FC<ModalProps> = ({ message, onClose }) => {
+const LoginCheckFalseModal: React.FC<ModalProps> = ({
+  alertmessage,
+  onClose,
+}) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded shadow-lg text-center">
-        <p>{message}</p>
+        <p>{alertmessage}</p>
         <button
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
           onClick={onClose}
