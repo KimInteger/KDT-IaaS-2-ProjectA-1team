62cf3d2161f334bb9cb6f4026364900d8346ed6e
김정수 <kjs20151240@gmail.com>
2024-07-26 11:07:21 +0900

    :recycle: 김정수 : 조건 추가

--- frontnext/src/app/ui/testComponent/InputMessage.tsx
+++ frontnext/src/app/ui/testComponent/InputMessage.tsx
@@ -6,12 +6,13 @@ interface MessageProps {
   isSuccess: boolean;
 }
 
-const Message: React.FC<MessageProps> = ({ message, isSuccess }) => (
-  <div className={styles.messageContainer}>
-    <h1 className={isSuccess ? styles.successText : styles.failureText}>
-      {message}
-    </h1>
-  </div>
-);
+const Message: React.FC<MessageProps> = ({ message, isSuccess }) => {
+  // isSuccess가 true일 때만 메시지를 렌더링
+  return isSuccess ? (
+    <div className={styles.messageContainer}>
+      <h1 className={styles.successText}>{message}</h1>
+    </div>
+  ) : null;
+};
 
 export default Message;
