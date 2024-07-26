ed92ca4289a83b00eb32ba7a3669466e38a43ca0
김정수 <kjs20151240@gmail.com>
2024-07-26 12:05:46 +0900

    :construction_worker: 김정수 : Header Test

--- frontnext/src/app/testField/page.tsx
+++ frontnext/src/app/testField/page.tsx
@@ -2,11 +2,18 @@ import Image from 'next/image';
 import '../globals.css';
 import Message from '../ui/testComponent/InputMessage';
 import { styles } from '../ui/testComponent/stylesContent';
+import TestHeader from '../ui/TestHeader';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Message message="Hellow World" isSuccess={true} />
+    <div>
+      <TestHeader />
+      <main>
+        <h1>Welcome to the Page</h1>
+        <div className={styles.container}>
+          <Message message="Hellow World" isSuccess={true} />
+        </div>
+      </main>
     </div>
   );
 }
