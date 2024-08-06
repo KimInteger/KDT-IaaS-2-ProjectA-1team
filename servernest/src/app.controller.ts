e7cc1a67b629800cbb95da58bcd3231648d038f4
김정수 <kjs20151240@gmail.com>
2024-08-06 15:29:09 +0900

    :sparkles: 김정수 : createRecommend요청 추가

--- servernest/src/app.controller.ts
+++ servernest/src/app.controller.ts
@@ -33,4 +33,21 @@ export class AppController {
       res.status(error.response?.status || 500).send(error.message || 'Error');
     }
   }
+
+  @Post('/createRecommend')
+  async makeResponse(@Body() body: string, @Res() res: Response) {
+    try {
+      const response = await fetch('http://localhost:8080/createRecommend', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      const data = await response.json();
+      res.status(response.status).send(data.success);
+    } catch (error) {
+      res.status(error.response?.status || 500).send(error.message || 'Error');
+    }
+  }
 }
