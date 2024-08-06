6e58e10b88d9cd4b1efaba936755c2eb95eb591b
김정수 <kjs20151240@gmail.com>
2024-08-06 09:28:25 +0900

    :egg: 김정수 : TableModal 호출

--- frontnext/src/app/TabSetting/page.tsx
+++ frontnext/src/app/TabSetting/page.tsx
@@ -1,10 +1,12 @@
 import HeaderSection from '../ui/Header/headerSection';
+import TableModal from '../ui/modal/components/tableModal';
 
 export default function Home() {
   return (
     <>
       <HeaderSection />
       <h1>Hellow TabSetting</h1>
+      <TableModal />
     </>
   );
 }
