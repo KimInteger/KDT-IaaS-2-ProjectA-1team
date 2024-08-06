a65e1870a9d20cf03357e4042c4af0bddfe7e439
김정수 <kjs20151240@gmail.com>
2024-08-06 18:10:50 +0900

    :fire: 김정수 : 코드삭제

--- frontnext/src/app/TabSetting/page.tsx
+++ frontnext/src/app/TabSetting/page.tsx
@@ -1,12 +1,10 @@
 import HeaderSection from '../ui/Header/headerSection';
-import TableModal from '../ui/modal/components/tableModal';
 
 export default function Home() {
   return (
     <>
       <HeaderSection />
       <h1>Hellow TabSetting</h1>
-      <TableModal />
     </>
   );
 }
