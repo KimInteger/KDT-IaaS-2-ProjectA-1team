3b4b193c5ef9a4e599117a3b8e866970bc35953b
김정수 <kjs20151240@gmail.com>
2024-08-12 15:37:59 +0900

    :recycle: 김정수 : 기존 코드로 돌려놓음

--- frontnext/src/app/ui/SettingMoules/UserManagement.tsx
+++ frontnext/src/app/ui/SettingMoules/UserManagement.tsx
@@ -1,14 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { SettingStyles } from '../styles/styles-ys';
 import PasswordChangeForm from './PWChangeForm';
-import BackupRestoreModal from './BackupRestoreModal';
 
 const UserManagement = () => {
-  const [isBackup, setIsBackup] = useState(false);
-
-  const openBackupModal = () => setIsBackup(true);
-  const closeBackupModal = () => setIsBackup(false);
-
   return (
     <div className={SettingStyles.contentStyle}>
       <div className="flex flex-col space-y-4">
@@ -18,12 +12,8 @@ const UserManagement = () => {
         <button className={SettingStyles.button}>사용자 권한 설정</button>
         <PasswordChangeForm />
         <button className={SettingStyles.button}>알림 설정</button>
-        <button className={SettingStyles.button} onClick={openBackupModal}>
-          백업 및 복구
-        </button>
+        <button className={SettingStyles.button}>백업 및 복구</button>
       </div>
-
-      {isBackup && <BackupRestoreModal onClose={closeBackupModal} />}
     </div>
   );
 };
